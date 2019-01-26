660448b25ffb8ecf335e60c3913de37ff0994fb2
rsantiagopaz <rsantiagopaz@gmail.com>
2019-01-26 07:16:30 -0300

    primer commit

---
+++ inventario/source/services/class/comp/Conexion.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$servidor = "localhost";
+$usuario = "root";
+$password = "";
+$base = "inventario_original";
+
+
+
+/*
+$servidor = "10.1.1.4";
+$usuario = "root";
+$password = "sum4mp4iea!";
+$base = "inventario_original";
+*/
+
+
+/*
+$servidor = "localhost";
+$usuario = "inventario";
+$password = "plata!salud!";
+$base = "inventario";
+*/
+
+?>
